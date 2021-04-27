1ecd96239ee2e2ec101b4164f919a47180e94c47
Martin Herold <martin3421@gmail.com>
2021-04-27 21:55:49 +0200

    initial commit

---
+++ src/app/wikipedia.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WikipediaService {
+  constructor() {}
+
+  search(term: string) {
+    return 'I am wikipedia search results';
+  }
+}
